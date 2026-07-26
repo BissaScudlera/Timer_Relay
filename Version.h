c73b3590084f65f4c6cb7a2c66cb991965290593
BissaScudlera <39187118+BissaScudlera@users.noreply.github.com>
2026-07-26 03:02:43 +0200

    Create Version.h

---
+++ Version.h
@@ -0,0 +1,14 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define FW_NAME        "Timer Relay ESP32"
+#define FW_MAJOR       1
+#define FW_MINOR       1
+#define FW_PATCH       0
+
+#define FW_BUILD_DATE  __DATE__
+#define FW_BUILD_TIME  __TIME__
+
+#define FW_STRING  "1.1.0"
+
+#endif
