e2c97439c044638ea0b5e540ba189b4e45e76319
BissaScudlera <39187118+BissaScudlera@users.noreply.github.com>
2026-08-02 21:19:13 +0200

    Update Version.h

--- Version.h
+++ Version.h
@@ -9,7 +9,7 @@
 
 #define FW_MAJOR           1
 #define FW_MINOR           9
-#define FW_PATCH           0
+#define FW_PATCH           1
 
 // Incrementare ad ogni compilazione ufficiale
 #define FW_BUILD           1
